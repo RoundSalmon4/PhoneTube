d43e97e143a0ec79d210369cd03db55d01aa8089
roundsalmon4 <209016228+RoundSalmon4@users.noreply.github.com>
2026-07-29 10:24:10 -0500

    fix: add missing ExperimentalFoundationApi import

--- app/src/main/java/com/roundsalmon4/phonetube/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/roundsalmon4/phonetube/ui/settings/SettingsScreen.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
