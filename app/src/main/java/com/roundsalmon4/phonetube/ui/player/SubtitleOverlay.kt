f983ab5e5d07aaf3a07c08256d130a0097e96e30
roundsalmon4 <209016228+RoundSalmon4@users.noreply.github.com>
2026-08-02 14:19:39 -0500

    fix: correct TextShadow import package

--- app/src/main/java/com/roundsalmon4/phonetube/ui/player/SubtitleOverlay.kt
+++ app/src/main/java/com/roundsalmon4/phonetube/ui/player/SubtitleOverlay.kt
@@ -13,9 +13,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextShadow
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextShadow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.media3.common.Player
