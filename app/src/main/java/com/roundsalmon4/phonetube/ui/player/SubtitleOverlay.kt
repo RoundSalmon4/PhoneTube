57eb619ddd9e092ff0b2d7b2d2a6a78cc33acfc2
roundsalmon4 <209016228+RoundSalmon4@users.noreply.github.com>
2026-08-02 14:01:01 -0500

    fix: correct TextAlign import package

--- app/src/main/java/com/roundsalmon4/phonetube/ui/player/SubtitleOverlay.kt
+++ app/src/main/java/com/roundsalmon4/phonetube/ui/player/SubtitleOverlay.kt
@@ -13,8 +13,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextAlign
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextShadow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
