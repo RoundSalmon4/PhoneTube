d1181baa882ac2f7af618356043ec5bc90702e42
roundsalmon4 <209016228+RoundSalmon4@users.noreply.github.com>
2026-09-16 18:09:09 -0500

    fix: import SubtitleTrack in PlayerViewModel

--- app/src/main/java/com/roundsalmon4/phonetube/ui/player/PlayerViewModel.kt
+++ app/src/main/java/com/roundsalmon4/phonetube/ui/player/PlayerViewModel.kt
@@ -22,6 +22,7 @@ import com.roundsalmon4.phonetube.core.engine.XtreamClient
 import com.roundsalmon4.phonetube.core.engine.YouTubeEngine
 import com.roundsalmon4.phonetube.core.engine.model.SponsorSegment
 import com.roundsalmon4.phonetube.core.engine.model.StreamFormat
+import com.roundsalmon4.phonetube.core.engine.model.SubtitleTrack
 import com.roundsalmon4.phonetube.core.engine.model.bestCastUrl
 import com.roundsalmon4.phonetube.core.engine.model.StreamInfo
 import com.roundsalmon4.phonetube.core.engine.model.VideoChapter
